2e6ef0a44cb6c1e357a60f700b6dcb9955c41f0a
JuanPaS <juan.sanchez@newshore.es>
2018-06-20 22:00:00 -0500

    EditPackage

--- src/components/EditPackage.vue
+++ src/components/EditPackage.vue
@@ -1,36 +1,55 @@
 <template>
     <div>
-        <h1>Edit Package</h1>
-        <div class="row">
-            <div class="col-md-10"></div>
-            <div class="col-md-2">
-                <router-link :to="{ name: 'ListProducts' }" class="btn btn-success">Back</router-link>
+        <h1 class="text-center">Edit Package</h1>
+        <br>
+        <form v-on:submit.prevent="editPackage">
+            <div class="row">
+                <div class="col-md-5 text-center">
+                    <img id="photo" width="100%" :src="package.imageUrl">
+                    <input type="file" name="" id="">
+                </div>
+                <div class="col-md-7">
+                    <div class="form-group">
+                        <label>
+                            <strong>From person from</strong>
+                        </label>
+                        <input type="text" class="form-control" v-model="package.fromPersonName">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <strong>To person name</strong>
+                        </label>
+                        <input type="text" class="form-control" v-model="package.toPersonName">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <strong>Phone</strong>
+                        </label>
+                        <input type="text" class="form-control" v-model="package.phone">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <strong>To address</strong>
+                        </label>
+                        <input type="text" class="form-control" v-model="package.toAddress">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <strong>Weight</strong>
+                        </label>
+                        <input type="number" class="form-control" v-model="package.weight">
+                    </div>
+                </div>
             </div>
-        </div>
-        <form v-on:submit.prevent="editProduct">
-            <div class="form-group">
-                <label>
-                    <strong>From person from</strong>
-                </label>
-                <input type="text" class="form-control" v-model="package.fromPersonName">
-            </div>
-            <div class="form-group">
-                <label>
-                    <strong>To person name</strong>
-                </label>
-                <input type="text" class="form-control" v-model="package.toPersonName">
-            </div>
-            <div class="form-group">
-                <label>
-                    <strong>Phone</strong>
-                </label>
-                <input type="date" class="form-control" v-model="package.phone">
-            </div>
-            <img id="photo" alt="" :src="package.imageUrl">
-            <div class="form-group">
+            <div class="form-group text-center">
                 <button class="btn btn-info">Edit</button>
             </div>
         </form>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <router-link :to="{ name: 'ListProducts' }" class="btn btn-success">Back</router-link>
+            </div>
+        </div>
     </div>
 </template>
 <script>
