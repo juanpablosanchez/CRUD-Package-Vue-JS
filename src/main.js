9ea0c56b021da36fb4c78acabd6d2fb1d073f99d
JuanPaS <juan.sanchez@newshore.es>
2018-06-22 22:05:08 -0500

    Reset app and remove bootstrap

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import BootstrapVue from "bootstrap-vue";
-import App from './App';
-import router from './router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.use(VueAxios, axios);
-Vue.use(BootstrapVue);
 Vue.use(require('vue-moment'));
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
 Vue.mixin({
   data: function() {
@@ -22,11 +17,12 @@ Vue.mixin({
     }
   }
 })
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-});
+  components: { App },
+  template: '<App/>'
+})
