f6b8968eda535409414b09cc8992888ab2efb655
JuanPaS <juan.sanchez@newshore.es>
2018-06-22 22:19:58 -0500

    Install vuetifyjs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-Vue.use(VueAxios, axios);
-Vue.use(require('vue-moment'));
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import Vuetify from 'vuetify'
 
+import 'babel-polyfill'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(VueAxios, axios);
+Vue.use(require('vue-moment'));
+Vue.use(Vuetify)
 Vue.mixin({
   data: function() {
     return {
